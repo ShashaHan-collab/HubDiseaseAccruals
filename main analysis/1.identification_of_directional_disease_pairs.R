99ce54706fd04a4d3f03dd6296fcedc560270866
sin6666666 <lisairan_bnu@163.com>
2025-11-04 13:50:41 +0800

    Update 1.identification_of_directional_disease_pairs.R

--- main analysis/1.identification_of_directional_disease_pairs.R
+++ main analysis/1.identification_of_directional_disease_pairs.R
@@ -1,16 +1,16 @@
 library(parallel)
 
-### 将多次住院经历合并
+### Combine multiple hospitalization records
 var_disease_code_list_combine_uni <- vector(mode = "list", length = length(unique(var_match)))
 names(var_disease_code_list_combine_uni) <- unique(var_match)
 var_match_tab <- table(var_match)
 logi <- var_match_tab >= 2
-var_match_once <- names(var_match_tab) [!logi]
-var_match_more <- names(var_match_tab) [logi]
-## 单次住院不变
+var_match_once <- names(var_match_tab)[!logi]
+var_match_more <- names(var_match_tab)[logi]
+## Keep single hospitalization records unchanged
 index_once <- index[var_match_once]
 var_disease_code_list_combine_uni[var_match_once] <- var_disease_code_list_uni[index_once]
-## 多次住院合并
+## Combine multiple hospitalization records
 for(i in 1:length(var_match_more)) {
   var_match_more_i <- var_match_more[i]
   index_more_i <- index[names(index) %in% var_match_more_i]
@@ -18,13 +18,13 @@ for(i in 1:length(var_match_more)) {
   var_disease_code_list_combine_uni[[var_match_more_i]] <- temp
 }
 
-### 考虑有单次和多次住院经历的病人的疾病代码出现频率
+### Consider the frequency of disease codes in patients with single and multiple hospitalizations
 var_disease_code_lib_onceANDmore <- 
-  sort(unique(do.call("c", var_disease_code_list_combine_uni)))## 疾病代码库
+  sort(unique(do.call("c", var_disease_code_list_combine_uni)))## Disease code library
 var_disease_code_tab_onceANDmore <- 
-  table(do.call("c", var_disease_code_list_combine_uni))## 疾病代码出现频率
-logi <- var_disease_code_tab_onceANDmore >= 100 ## 考虑所有病人中曾患过的次数不少于100次的疾病代码
-var_disease_code_chosen <- names(var_disease_code_tab_onceANDmore) [logi]
+  table(do.call("c", var_disease_code_list_combine_uni))## Frequency of disease codes
+logi <- var_disease_code_tab_onceANDmore >= 100 ## Consider disease codes that occurred at least 100 times in all patients
+var_disease_code_chosen <- names(var_disease_code_tab_onceANDmore)[logi]
 var_disease_code_chosen <- var_disease_code_chosen[!grepl("^BN", var_disease_code_chosen)]
 logi <- (var_disease_code_chosen >= "S00" & var_disease_code_chosen <= "T98") |
   (var_disease_code_chosen >= "V01" & var_disease_code_chosen <= "Y98")
@@ -37,40 +37,36 @@ get_date <- function(x) {
   as.Date(x)
 }
 
-############################ 预处理：创建索引 与 匹配 #################################
-RY_date <- get_date(var_time) ## 入院日期
-## 将入院时间按月份分为12部分
-#RY_month <- months.Date(RY_date)
+############################ Preprocessing: Create indices and matching #################################
+RY_date <- get_date(var_time) ## Admission date
 RY_quarters <- quarters(RY_date)
 RY_year <- substr(var_time, 1, 4)##  2013 ~ 2021
-XB <- var_num[, 2] ## 性别
-NL <- var_num[, 3] ## 年龄
-## 将年龄分为4部分： 新生儿（NL == 0），未成年（0 < NL <= 19），青年（20 <= NL < 55），中老年（NL >= 55）
+XB <- var_num[, 2] ## Gender
+NL <- var_num[, 3] ## Age
+## Divide age into 4 groups: Newborns (NL == 0), Minors (0 < NL <= 19), Young adults (20 <= NL < 55), Middle-aged and elderly (NL >= 55)
 NL <- 0 * (NL == 0) + 1 * (NL > 0 & NL <= 19) + 2 * (NL >= 20 & NL < 55) + 3 * (NL >= 55)
 
-## 有哪些入院记录中会患 对应的病
+## Which admission records are associated with corresponding diseases
 all_index_get_disease <- vector("list", d); names(all_index_get_disease) <- var_disease_code_chosen
 for(code in var_disease_code_chosen) {
   logi <- do.call("c", lapply(var_disease_code_list_uni, function(x, code) {code %in% x}, code = code))
   all_index_get_disease[[code]] <- index[logi]
 }
 
-## 同一位病人5年内的入院记录 索引
+## Indices of admission records for the same patient within 5 years
 all_index_within_5 <- vector("list", length(var_match)); names(all_index_within_5) <- var_match
 var_match_tab <- table(var_match)
-temp <- names(var_match_tab) [var_match_tab > 1]
-for(i in which(var_match %in% temp)) { times<-times+1;print(times)
+temp <- names(var_match_tab)[var_match_tab > 1]
+for(i in which(var_match %in% temp)) { times <- times + 1; print(times)
 logi_temp_i <- var_match == var_match[i] & as.numeric(RY_date - RY_date[i]) >= 0 &
   as.numeric(RY_date - RY_date[i]) <= 365 * 5
 all_index_within_5[[i]] <- index[logi_temp_i]
 }
-temp <- names(var_match_tab) [var_match_tab == 1]
+temp <- names(var_match_tab)[var_match_tab == 1]
 all_index_within_5[temp] <- as.list(index[temp])
 
-## 为每一条入院记录先做好匹配
+## Pre-match for each admission record
 all_index_matched <- vector("list", length(var_match))
-#char_month <- c("一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", 
-#                "十月", "十一月", "十二月")
 char_quarters <- c("Q1", "Q2", "Q3", "Q4")
 char_year <- sort(unique(RY_year))
 #char_group <- matrix("", nrow = length(char_year) * 12 * 2 * length(unique(NL)), ncol = 4)
@@ -83,11 +79,9 @@ for(i_year in char_year) {
   for(i_quarters in char_quarters) {
     for(i_XB in 0:1) {
       for(i_NL in sort(unique(NL))) {
-        #logi_temp_i <- RY_year == i_year & RY_month == i_month & XB == i_XB & NL == i_NL
         logi_temp_i <- RY_year == i_year & RY_quarters == i_quarters & XB == i_XB & NL == i_NL
         if(!any(logi_temp_i)) next
         group[logi_temp_i] <- group_num
-        #char_group[group_num, ] <- c(i_year, i_month, i_XB, i_NL)
         char_group[group_num, ] <- c(i_year, i_quarters, i_XB, i_NL)
         group_num <- group_num + 1; print(group_num)
       }
@@ -95,23 +89,21 @@ for(i_year in char_year) {
   }
 }
 char_group <- char_group[1:(group_num-1), ]
-group_tab <- table(group); group_tab; min(group_tab) # 一组至少有 1 条住院记录
-write.csv(cbind(table(group), char_group), "匹配后的组数及每组的住院记录数.csv", fileEncoding = "utf8")
-
-##  logi <- group_tab >= 20; 
+group_tab <- table(group); group_tab; min(group_tab) # Each group has at least 1 hospitalization record
+write.csv(cbind(table(group), char_group), "Number of matched groups and hospitalization records per group.csv", fileEncoding = "utf8")
 
 Pr_D2_all_group <- matrix(0, nrow = length(unique(group)), ncol = length(var_disease_code_chosen))
 colnames(Pr_D2_all_group) <- var_disease_code_chosen
 for(i_group in sort(unique(group))) {
   index_i <- which(group == i_group)
   n_match_i <- length(index_i) ## n_Match
-  ## 利用上述样本获取 Pr(D2)
+  ## Obtain Pr(D2) using the above samples
   Pr_D2_i <- rep(0, length(var_disease_code_chosen))
   names(Pr_D2_i) <- var_disease_code_chosen
   for(j in 1:n_match_i) {
-    c_i_D2 <- rep(0, length(var_disease_code_chosen)) ## 上述匹配样本中，5年内患有D2的数量
+    c_i_D2 <- rep(0, length(var_disease_code_chosen)) ## Number of cases with D2 within 5 years in the matched samples
     names(c_i_D2) <- var_disease_code_chosen
-    ## 病人在 5年内 患有D2，则 计数+1
+    ## If the patient had D2 within 5 years, increment the count by 1
     index_j <- all_index_within_5[[index_i[j]]]
     for(D2 in var_disease_code_chosen) {
       c_i_D2[D2] <- c_i_D2[D2] + any(index_j %in% all_index_get_disease[[D2]])
@@ -124,33 +116,33 @@ for(i_group in sort(unique(group))) {
 
 save.image("match_group.RData")
 
-############################ get RR and p_value #################################
+############################ Get RR and p_value #################################
 main <- function(D1, var_disease_code_chosen, all_index_get_disease, all_index_within_5, group, 
                  Pr_D2_all_group, index) {
   tryCatch(
     {
-      ## 获取所有患有 D1 的样本 （exposed group）
+      ## Get all samples with D1 (exposed group)
       index_contain_D1 <- all_index_get_disease[[D1]]
       if(is.null(index_contain_D1) | length(index_contain_D1) == 0) {
         p_value_binom_D1 <- rep(-1, length(var_disease_code_chosen)); names(p_value_binom_D1) <- var_disease_code_chosen
         RR_binom_D1 <- rep(-1, length(var_disease_code_chosen)); names(RR_binom_D1) <- var_disease_code_chosen
         return(list("p_value" = p_value_binom_D1, "RR" = RR_binom_D1))
       }
-      n_discharges <- length(index_contain_D1) ## 患有 D1 的样本量
+      n_discharges <- length(index_contain_D1) ## Sample size with D1
       index_D1 <- index[index_contain_D1]
       C_exposed_D2 <- rep(0, length(var_disease_code_chosen) - 1) ## C_exposed
       names(C_exposed_D2) <- setdiff(var_disease_code_chosen, D1)
       Pr_D2_test <- rep(0, length(var_disease_code_chosen) - 1)   ## Pr(D2)_test
       names(Pr_D2_test) <- setdiff(var_disease_code_chosen, D1)
       for(i in 1:n_discharges) {
-        ## 暴露组 第i个样本, 5年内是否患有D2
+        ## For the i-th sample in the exposed group, check if D2 occurred within 5 years
         index_temp_i <- all_index_within_5[[index_D1[i]]]
         for(D2 in setdiff(var_disease_code_chosen, D1)) {
           C_exposed_D2[D2] <- C_exposed_D2[D2] + any(index_temp_i %in% all_index_get_disease[[D2]])
         }
       }
       
-      ## 将 n_discharges 按 group 分别计算
+      ## Calculate n_discharges by group
       group_contain_D1 <- group[index_contain_D1]
       group_count <- rep(0, length(unique(group)))
       names(group_count) <- as.character(1:length(unique(group)))
@@ -185,7 +177,7 @@ main <- function(D1, var_disease_code_chosen, all_index_get_disease, all_index_w
 }
 
 
-#################################### 按性别分开处理 ####################################
+#################################### Process separately by gender ####################################
 d <- length(var_disease_code_chosen)
 RR <- matrix(0, d, d); colnames(RR) <- rownames(RR) <- var_disease_code_chosen
 p_value_binom_XB0 <- RR_binom_XB0 <- p_value_binom_XB1 <- RR_binom_XB1 <- RR
@@ -198,36 +190,35 @@ all_index_get_disease_XB1 <- lapply(all_index_get_disease, FUN = intersect, y = 
 ### XB = 0
 t1 <- Sys.time()
 cl <- makeCluster(2)
-a <- parallel::parLapply(cl=cl, var_disease_code_chosen, main, var_disease_code_chosen = var_disease_code_chosen, 
+a <- parallel::parLapply(cl = cl, var_disease_code_chosen, main, var_disease_code_chosen = var_disease_code_chosen, 
                          all_index_get_disease = all_index_get_disease_XB0, 
                          all_index_within_5 = all_index_within_5, group = group, 
                          Pr_D2_all_group = Pr_D2_all_group, index = index)
 stopCluster(cl); gc()
-t2 <- Sys.time(); t2-t1
+t2 <- Sys.time(); t2 - t1
 
 for(i in 1:length(var_disease_code_chosen)) {
   D1 <- var_disease_code_chosen[i]
-  p_value_binom_XB0[D1, ] <- a[[i]] [["p_value"]]
-  RR_binom_XB0[D1, ] <- a[[i]] [["RR"]]
+  p_value_binom_XB0[D1, ] <- a[[i]][["p_value"]]
+  RR_binom_XB0[D1, ] <- a[[i]][["RR"]]
 }
 remove("a")
 
 ### XB = 1
 t1 <- Sys.time()
 cl <- makeCluster(2)
-a <- parallel::parLapply(cl=cl, var_disease_code_chosen, main, var_disease_code_chosen = var_disease_code_chosen, 
+a <- parallel::parLapply(cl = cl, var_disease_code_chosen, main, var_disease_code_chosen = var_disease_code_chosen, 
                          all_index_get_disease = all_index_get_disease_XB1, 
                          all_index_within_5 = all_index_within_5, group = group, 
                          Pr_D2_all_group = Pr_D2_all_group, index = index)
 stopCluster(cl); gc()
-t2 <- Sys.time(); t2-t1
+t2 <- Sys.time(); t2 - t1
 
 for(i in 1:length(var_disease_code_chosen)) {
   D1 <- var_disease_code_chosen[i]
-  p_value_binom_XB1[D1, ] <- a[[i]] [["p_value"]]
-  RR_binom_XB1[D1, ] <- a[[i]] [["RR"]]
+  p_value_binom_XB1[D1, ] <- a[[i]][["p_value"]]
+  RR_binom_XB1[D1, ] <- a[[i]][["RR"]]
 }
 remove("a")
 
 save.image("p_value and RR for binomial model_par_XBsep.RData")
-
