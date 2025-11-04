152a63b56d10dbc2c26e8d096d4c05e680f8abb8
sin6666666 <lisairan_bnu@163.com>
2025-11-04 13:53:39 +0800

    Update 2.directionality_testing.R

--- main analysis/2.directionality_testing.R
+++ main analysis/2.directionality_testing.R
@@ -1,7 +1,5 @@
 rm(list = ls())
 load("p_value and RR for binomial model_par_XBsep.RData")
-
-
 for(XB_aim in 0:1) {
   csvname_all <- paste0("D1_to_D2_XB", XB_aim, ".csv")
   csvname_Nsame_rm <- paste0("D1_to_D2_XB", XB_aim, "Nsame_rm.csv")
@@ -11,17 +9,15 @@ for(XB_aim in 0:1) {
   RR_binom <- get(paste0("RR_binom_XB", XB_aim))
   
   logi <- RR_binom > 1 & p_value_binom <= 0.05
-  #length(var_disease_code_chosen)
-  #dim(RR_binom)
   temp1 <- matrix(var_disease_code_chosen, ncol = length(var_disease_code_chosen), nrow = length(var_disease_code_chosen))
   temp2 <- t(temp1)
-  D1 <- temp1[logi]; D2 <- temp2[logi]## ------------- D1 -> D2
+  D1 <- temp1[logi]; D2 <- temp2[logi]##  D1 -> D2
   D1_to_D2 <- data.frame("V1" = D1, "V2" = D2, stringsAsFactors = F)
   D2_to_D1 <- data.frame("V1" = D2, "V2" = D1, stringsAsFactors = F)
   temp <- rbind(D1_to_D2, D2_to_D1)
   temp1_temp2 <- paste(temp[,1], temp[,2], sep = "_")
   temp1_temp2_tab <- table(temp1_temp2)
-  char <- names(temp1_temp2_tab) [temp1_temp2_tab > 1]
+  char <- names(temp1_temp2_tab)[temp1_temp2_tab > 1]
   logi <- paste(D1_to_D2[,1], D1_to_D2[,2], sep = "_") %in% char
   D1_to_D2_double <- D1_to_D2[logi,]
   D1_to_D2_single <- D1_to_D2[!logi,]
@@ -41,9 +37,9 @@ for(XB_aim in 0:1) {
     index_contain_D2 <- all_index_get_disease[[D2]]
     index_D1 <- index[index_contain_D1]
     index_D2 <- index[index_contain_D2]
-    var_match_D1 <- names(index_D1)##  患有D1的病人编号
-    var_match_D2 <- names(index_D2)##  患有D2的病人编号
-    ## 只保留同时患有D1，D2的病人编号
+    var_match_D1 <- names(index_D1)## Patient IDs with D1
+    var_match_D2 <- names(index_D2)## Patient IDs with D2
+    ## Keep only patient IDs with both D1 and D2
     var_match_common <- intersect(var_match_D1, var_match_D2)
     N <- length(var_match_common)
     index_common_D1 <- index_D1[var_match_common]
@@ -51,7 +47,7 @@ for(XB_aim in 0:1) {
     N_D1 <- sum(index_common_D1 < index_common_D2)
     N_D2 <- sum(index_common_D1 > index_common_D2)
     N_same <- sum(index_common_D1 == index_common_D2)
-    #### N_same 不特殊处理
+    #### No special handling for N_same
     p_value_D1_to_D2 <- binom.test(N_D1, N, p = 0.5, alternative = "greater")$p.value
     p_value_D2_to_D1 <- binom.test(N_D2, N, p = 0.5, alternative = "greater")$p.value
     if(p_value_D1_to_D2 < 0.025) {
@@ -59,11 +55,11 @@ for(XB_aim in 0:1) {
     } else if(p_value_D2_to_D1 < 0.025) {
       D1_to_D2_double_uni_list[[i]] <- data.frame("D1" = D2, "D2" = D1, stringsAsFactors = F)
     }
-    #### N_same 特殊处理
-    ## N_same 直接删除
-    if(N_D1+N_D2 > 0) {
-      p_value_D1_to_D2_Nsame_rm <- binom.test(N_D1, N_D1+N_D2, p = 0.5, alternative = "greater")$p.value
-      p_value_D2_to_D1_Nsame_rm <- binom.test(N_D2, N_D1+N_D2, p = 0.5, alternative = "greater")$p.value
+    #### Special handling for N_same
+    ## Remove N_same directly
+    if(N_D1 + N_D2 > 0) {
+      p_value_D1_to_D2_Nsame_rm <- binom.test(N_D1, N_D1 + N_D2, p = 0.5, alternative = "greater")$p.value
+      p_value_D2_to_D1_Nsame_rm <- binom.test(N_D2, N_D1 + N_D2, p = 0.5, alternative = "greater")$p.value
     } else {
       p_value_D1_to_D2_Nsame_rm <- p_value_D2_to_D1_Nsame_rm <- 1
     }
@@ -72,8 +68,8 @@ for(XB_aim in 0:1) {
     } else if(p_value_D2_to_D1_Nsame_rm < 0.025) {
       D1_to_D2_double_uni_Nsame_rm_list[[i]] <- data.frame("D1" = D2, "D2" = D1, stringsAsFactors = F)
     }
-    ## N_same 按比例分割
-    if(N_D1+N_D2 > 0) {
+    ## Split N_same proportionally
+    if(N_D1 + N_D2 > 0) {
       temp1 <- round(N_same * (N_D1 / (N_D1 + N_D2)), digits = 0)
       temp2 <- N_same - temp1
     } else {
@@ -98,9 +94,9 @@ for(XB_aim in 0:1) {
     index_contain_D2 <- all_index_get_disease[[D2]]
     index_D1 <- index[index_contain_D1]
     index_D2 <- index[index_contain_D2]
-    var_match_D1 <- names(index_D1)##  患有D1的病人编号
-    var_match_D2 <- names(index_D2)##  患有D2的病人编号
-    ## 只保留同时患有D1，D2的病人编号
+    var_match_D1 <- names(index_D1)## Patient IDs with D1
+    var_match_D2 <- names(index_D2)## Patient IDs with D2
+    ## Keep only patient IDs with both D1 and D2
     var_match_common <- intersect(var_match_D1, var_match_D2)
     N <- length(var_match_common)
     index_common_D1 <- index_D1[var_match_common]
@@ -108,7 +104,7 @@ for(XB_aim in 0:1) {
     N_D1 <- sum(index_common_D1 < index_common_D2)
     N_D2 <- sum(index_common_D1 > index_common_D2)
     N_same <- sum(index_common_D1 == index_common_D2)
-    #### N_same 不特殊处理
+    #### No special handling for N_same
     p_value_D1_to_D2 <- binom.test(N_D1, N, p = 0.5, alternative = "greater")$p.value
     p_value_D2_to_D1 <- binom.test(N_D2, N, p = 0.5, alternative = "greater")$p.value
     if(p_value_D1_to_D2 < 0.025) {
@@ -116,11 +112,11 @@ for(XB_aim in 0:1) {
     } else if(p_value_D2_to_D1 < 0.025) {
       D1_to_D2_single_list[[i]] <- data.frame("D1" = D2, "D2" = D1, stringsAsFactors = F)
     }
-    #### N_same 特殊处理
-    ## N_same 直接删除
-    if(N_D1+N_D2 > 0) {
-      p_value_D1_to_D2_Nsame_rm <- binom.test(N_D1, N_D1+N_D2, p = 0.5, alternative = "greater")$p.value
-      p_value_D2_to_D1_Nsame_rm <- binom.test(N_D2, N_D1+N_D2, p = 0.5, alternative = "greater")$p.value
+    #### Special handling for N_same
+    ## Remove N_same directly
+    if(N_D1 + N_D2 > 0) {
+      p_value_D1_to_D2_Nsame_rm <- binom.test(N_D1, N_D1 + N_D2, p = 0.5, alternative = "greater")$p.value
+      p_value_D2_to_D1_Nsame_rm <- binom.test(N_D2, N_D1 + N_D2, p = 0.5, alternative = "greater")$p.value
     } else {
       p_value_D1_to_D2_Nsame_rm <- p_value_D2_to_D1_Nsame_rm <- 1
     }
@@ -129,8 +125,8 @@ for(XB_aim in 0:1) {
     } else if(p_value_D2_to_D1_Nsame_rm < 0.025) {
       D1_to_D2_single_Nsame_rm_list[[i]] <- data.frame("D1" = D2, "D2" = D1, stringsAsFactors = F)
     }
-    ## N_same 按比例分割
-    if(N_D1+N_D2 > 0) {
+    ## Split N_same proportionally
+    if(N_D1 + N_D2 > 0) {
       temp1 <- round(N_same * (N_D1 / (N_D1 + N_D2)), digits = 0)
       temp2 <- N_same - temp1
     } else {
